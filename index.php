fbf98eb13d0fd759f78942f773c6c8364decbe84
Johanne <johanne_lewis@hotmail.com>
2019-07-07 14:14:42 +0100

    Tidy up code

--- index.php
+++ index.php
@@ -9,94 +9,75 @@
   <body>    
 
     <form enctype="multipart/form-data" action="" method="POST">
-
-    <!--    <input type="hidden" name="MAX_FILE_SIZE" value="30000" /> -->
-
-        Process this file: <input name="userfile" type="file" required />
+       Process this file: <input name="userfile" type="file" required />
        <input type="submit" name="submit" value="Process File" />
     </form>
 
-
-<?php 
-      
-    if(isset($_POST['submit']))
-    {      
-      $user_file = $_FILES['userfile']['name'];
-      $user_file_temp = $_FILES['userfile']['tmp_name'];
-
-       move_uploaded_file($user_file_temp, "files/$user_file");
-
+<?php    
+  if(isset($_POST['submit']))
+   {      
+     $user_file = $_FILES['userfile']['name'];
+     $user_file_temp = $_FILES['userfile']['tmp_name'];
+       
+     move_uploaded_file($user_file_temp, "files/$user_file");
+       
      $filename = "files/$user_file";
-        $handle = fopen($filename, "r");
+     $handle = fopen($filename, "r");
     
-  if($handle) 
-     {
-      
-      //echo count($handle);
-           
-      $count = 0;
-      echo "<table class='sortable'>";
+     if($handle) 
+      {  
+        $count = 0;
+        echo "<hr><h2><strong>Please click on column heading to sort.</strong></h2>";
+        echo "<table class='sortable'>";
         while (($line = fgets($handle)) !== false)
-         {
-            
+          { 
             if($count == 0)
-              { 
-                echo "<thead><tr>";
-                $columns = explode("|",$line);
-                  $i = 1;          
-                foreach($columns as $column)
-                 {
-                   echo "<th> " .$i.". " .$column. "</th>";
-                    $i++;
-                 }
-                echo "</tr></thead>";
-              }
+             { 
+               echo "<thead><tr>";
+               $columns = explode("|",$line);
+               $i = 1;          
+               foreach($columns as $column)
+                {
+                  echo "<th> " .$i.". " .$column. "</th>";
+                  $i++;
+                }
+               echo "</tr></thead>";
+             }
             else
              {
-                if($count == 1)
-                 {
-                    echo "<tbody>"; 
-                    echo "<tr>";
-                    $rows = explode("|",$line);            
-                    foreach($rows as $row)
-                      {
-                        echo "<td>" .$row. "</th>";  
-                      }
-                     echo "</tr>";    
-                  }
-                else
-                 {                
-                    echo "<tr>";
-                    $rows = explode("|",$line);            
-                    foreach($rows as $row)
-                     {
-                        echo "<td>" .$row. "</th>";  
-                     }
-                    echo "</tr>";
-                 }                 
-             }
-            
+               if($count == 1)
+                {
+                  echo "<tbody>"; 
+                  echo "<tr>";
+                  $rows = explode("|",$line);            
+                  foreach($rows as $row)
+                   {
+                     echo "<td>" .$row. "</th>";  
+                   }
+                  echo "</tr>";    
+                }
+               else
+                {                
+                  echo "<tr>";
+                  $rows = explode("|",$line);            
+                  foreach($rows as $row)
+                   {
+                     echo "<td>" .$row. "</th>";  
+                   }
+                  echo "</tr>";
+                }                 
+             }            
             $count++;
-         }
-
-        fclose($handle);
-      unlink( $filename);
-      echo "</tbody></table>";
-    } 
-  else 
-   {
-      // error opening the file.
-   } 
-  
-        
-            
-    }
-
-  
-
-
+          }
+         fclose($handle);
+         unlink( $filename);
+         echo "</tbody></table>";
+      } 
+     else 
+      {
+        // error opening the file.
+      }         
+   }
 ?>
-
-
   </body>
 </html>
